845077a9f1bde9e98ba2bc6a04b889625da8494f
hamou ait abderrahim <gh_aitabderrahim@esi.dz>
2025-04-11 18:52:21 -0400

    message changed

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -159,7 +159,7 @@ class ProductController {
       }
       const products = await Product.find({});
 
-      res.status(200).json(products);
+      res.status(200).json("MGL842");
     } catch (error) {
       logger.error("Server error")
       console.error(error);
